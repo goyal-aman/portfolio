581073e348f87d7c7075b9fd2a6f0ea8227158b9
goyal-aman <goyal.amanrocks@gmail.com>
2021-01-02 02:14:01 +0530

    changsd timeline app base url to ''

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('timeline/', include('timeline.urls')),
+    path('', include('timeline.urls')),
 ]
 
 if settings.DEBUG:
