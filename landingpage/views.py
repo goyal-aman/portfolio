b66e273797d2bff0ae22e61d7712068d30f5a070
goyal-aman <goyal.amanrocks@gmail.com>
2021-01-05 00:59:03 +0530

    added home-page view, url, page

--- landingpage/views.py
+++ landingpage/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import View
 
 # Create your views here.
+
+class HomePageView(View):
+    template_name = 'landingpage/home_page.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('timeline.urls')),
+    path('', include('landingpage.urls')),
+    path('timeline/', include('timeline.urls')),
 ]
 
 if settings.DEBUG:
