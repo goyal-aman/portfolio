500c619fa211016003ecdf6c8bfd48c8392ac43f
goyal-aman <goyal.amanrocks@gmail.com>
2021-01-06 04:22:11 +0530

    added default values to env vars in settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "&$jw*r_$iwu&stzhhzfk*2")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = os.getenv("DEBUG", False)
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*")
 
 
 # Application definition
