aa31272c5528eb035cca52fc740016493ad76dde
goyal-aman <goyal.amanrocks@gmail.com>
2021-01-09 14:38:27 +0530

    added url tests in landingpage app

--- landingpage/tests.py
+++ landingpage/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+#local imports 
+from portfolio.test import BaseTestUrl
+from landingpage import views as landingpage_views
 
 # Create your tests here.
+
+class TestHomeUrl(BaseTestUrl, TestCase):
+    url_name = 'home'
+    url_route = ''
+    view_class = landingpage_views.HomePageView
