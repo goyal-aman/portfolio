8df30727822d41b61a157afbeb45e6ead816486b
goyal-aman <goyal.amanrocks@gmail.com>
2021-01-09 15:27:13 +0530

    added model test for Card model

--- timeline/test.py
+++ timeline/test.py
@@ -1,8 +1,10 @@
 from django.test import TestCase 
+from mixer.backend.django import mixer
 
 #local imports 
 from portfolio.test import BaseTestUrl , BaseTestView
 from timeline import views as timeline_views 
+from timeline.models import Card
 
 class TestTimelineHomeUrl(BaseTestUrl, TestCase):
     url_name = 'timeline-home'
@@ -12,3 +14,33 @@ class TestTimelineHomeUrl(BaseTestUrl, TestCase):
 class TestCardView(BaseTestView, TestCase):
     url_name = 'timeline-home'
     template_name = 'timeline/card_list_view.html'
+
+class TestCardModel(TestCase):
+    def setUp(self) -> None:
+        self.card1 = mixer.blend('timeline.Card')
+        self.card2 = mixer.blend('timeline.Card')
+
+    def test_card_count(self):
+        self.assertTrue(Card.objects.count() == 2)
+    
+    def test_card_update(self):
+        card1 = Card.objects.first()
+        card2 = Card.objects.last()
+        
+        card1.heading = "new_heading1"
+        card1.body = "new_body1"
+
+        card2.heading = "new_heading2"
+        card2.body = "new_body2"
+
+        card1.save()
+        card2.save()
+
+        card1 = Card.objects.first()
+        card2 = Card.objects.last() 
+
+        self.assertTrue(card1.body=="new_body1")
+        self.assertTrue(card1.heading=="new_heading1")
+    
+        self.assertTrue(card2.body=="new_body2")
+        self.assertTrue(card2.heading=="new_heading2")
