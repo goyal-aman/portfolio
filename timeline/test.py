ad2f780261389e39e02f9afbe8abc7d0b3417f82
goyal-aman <goyal.amanrocks@gmail.com>
2021-01-09 15:07:46 +0530

    added view tests for timeline app

--- timeline/test.py
+++ timeline/test.py
@@ -1,10 +1,14 @@
 from django.test import TestCase 
 
 #local imports 
-from portfolio.test import BaseTestUrl 
+from portfolio.test import BaseTestUrl , BaseTestView
 from timeline import views as timeline_views 
 
 class TestTimelineHomeUrl(BaseTestUrl, TestCase):
     url_name = 'timeline-home'
     url_route = 'timeline/'
     view_class = timeline_views.CardView
+
+class TestCardView(BaseTestView, TestCase):
+    url_name = 'timeline-home'
+    template_name = 'timeline/card_list_view.html'
