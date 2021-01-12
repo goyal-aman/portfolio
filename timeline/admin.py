a6515584b45458c2e2181d89cb8ec4e6937ccdda
goyal-aman <goyal.amanrocks@gmail.com>
2021-01-12 16:50:22 +0530

    added link in timeline card

--- timeline/admin.py
+++ timeline/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # local imports 
-from timeline.models import Card
+from timeline.models import Card, Link
 # Register your models here.
 admin.site.register(Card)
+admin.site.register(Link)
--- timeline/views.py
+++ timeline/views.py
@@ -2,7 +2,7 @@ from typing import List
 from django.shortcuts import render
 from django.views.generic import ListView, View
 # local imports
-from timeline.models import Card
+from timeline.models import Card, Link
 
 # general imports
 from collections import defaultdict
@@ -12,11 +12,14 @@ from collections import defaultdict
 class CardView(View):
     template_name = 'timeline/card_list_view.html'
     def get(self, request):
-        cards = Card.objects.all().order_by('-date').values()
+        cards = Card.objects.all().order_by('-date').prefetch_related('link').values()
         
         qs = defaultdict(list)
         for query in cards:
             card_year = query['date'].year
+            link_id = query.pop('link_id', None)
+            if link_id is not None:
+                query['link'] = Link.objects.get(id=link_id).url
             qs[card_year] += [query]
         qs = dict(qs)
         return render(request, template_name=self.template_name, context={'timeline_data':qs})
