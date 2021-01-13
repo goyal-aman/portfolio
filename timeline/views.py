82a12899aa17537c02fc3c1f7eb9322398b4ea6c
goyal-aman <goyal.amanrocks@gmail.com>
2021-01-13 13:54:52 +0530

    undid changes : until error cause is found

--- timeline/views.py
+++ timeline/views.py
@@ -12,14 +12,15 @@ from collections import defaultdict
 class CardView(View):
     template_name = 'timeline/card_list_view.html'
     def get(self, request):
-        cards = Card.objects.all().order_by('-date').prefetch_related('link').values()
+        # cards = Card.objects.all().order_by('-date').prefetch_related('link').values()
+        cards = Card.objects.all().order_by('-date').values()
         
         qs = defaultdict(list)
         for query in cards:
             card_year = query['date'].year
-            link_id = query.pop('link_id', None)
-            if link_id is not None:
-                query['link'] = Link.objects.get(id=link_id).url
+            # link_id = query.pop('link_id', None)
+            # if link_id is not None:
+                # query['link'] = Link.objects.get(id=link_id).url
             qs[card_year] += [query]
         qs = dict(qs)
         return render(request, template_name=self.template_name, context={'timeline_data':qs})
