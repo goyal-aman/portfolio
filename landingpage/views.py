2874b08f09eb41cdc22a6d37a6734dba17c0a1ee
goyal-aman <goyal.amanrocks@gmail.com>
2021-08-27 16:08:59 +0530

    added projects section

--- landingpage/views.py
+++ landingpage/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import View
 
+# models
+from landingpage.models import Project
+
 # Create your views here.
 
 class HomePageView(View):
     template_name = 'landingpage/index.html'
 
     def get(self, request):
-        return render(request, self.template_name)
+        qs = {'project_data':Project.objects.all()}
+        return render(request, self.template_name, context=qs)
 
 class ProjectView(View):
     template_name = 'landingpage/portfolio-details.html'
