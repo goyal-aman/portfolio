cdabb5a283cf9702f902e40e2fed07feab0cb8ef
goyal-aman <goyal.amanrocks@gmail.com>
2021-10-25 13:18:27 +0530

    added resume link on Hero section

--- landingpage/views.py
+++ landingpage/views.py
@@ -3,6 +3,7 @@ from django.views.generic import View
 
 # models
 from landingpage.models import Project
+from resume_pdf_section.models import ResumePdfLink
 
 # Create your views here.
 
@@ -10,9 +11,23 @@ class HomePageView(View):
     template_name = 'landingpage/index.html'
 
     def get(self, request):
-        qs = {'project_data':Project.objects.all()}
+        qs = {
+            "project_data": Project.objects.all(),
+            "data": {"link": self.get_resume_url(request)},
+        }
         return render(request, self.template_name, context=qs)
 
+    def get_resume_url(self, request):
+        try:
+            resumeObject:ResumePdfLink = ResumePdfLink.objects.first() 
+        except Exception as e:
+            # TODO : add feature to notify if resume is not fetched.
+            return None
+
+        if resumeObject:
+            return resumeObject.link
+        return None 
+        
 class ProjectView(View):
     template_name = 'landingpage/portfolio-details.html'
     def get(self, request):
