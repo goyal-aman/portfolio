cc80c004be0dff3428ab59ac6e26f09532be8261
goyal-aman <goyal.amanrocks@gmail.com>
2021-10-25 12:31:47 +0530

    created ResumePdfLink model

--- resume_pdf_section/models.py
+++ resume_pdf_section/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class ResumePdfLink(models.Model):
+    name = models.CharField(max_length=255)
+    link = models.URLField(blank=False, null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.name} @ {self.link[:30]}.. created_on{self.created_date}"
