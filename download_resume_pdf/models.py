322223b2f4f5dbb609c461b3707ece313a64fc9a
goyal-aman <goyal.amanrocks@gmail.com>
2021-10-29 10:03:45 +0000

    added fiture : automatically unset all other resumelink if current is set.

--- download_resume_pdf/models.py
+++ download_resume_pdf/models.py
@@ -10,5 +10,15 @@ class ResumeLink(models.Model):
     # TODO: there should only be one active resume at a time.
     is_active = models.BooleanField(default=False)
 
+    def save(self, *args, **kwargs):
+        # unset is_active of all other resume to false,
+        
+        if self.is_active:
+            activeResumes = ResumeLink.objects.filter(is_active=True)
+            for resume in activeResumes:
+                resume.is_active = False
+            ResumeLink.objects.bulk_update(activeResumes, ['is_active'])
+        super(ResumeLink, self).save(*args, **kwargs)
+
     def __str__(self):
         return f"{self.name[:30]}.. url:{self.url[:30]}"
